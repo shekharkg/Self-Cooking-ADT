5c0b97d451e3327f892c7f02dabb2438eeebdf29
ShekharKG <shekhar114@gmail.com>
2013-06-14 23:59:18 +0530

    new changes

--- sample/gen/com/astuetz/viewpager/extensions/sample/R.java
+++ sample/gen/com/astuetz/viewpager/extensions/sample/R.java
@@ -163,16 +163,15 @@ containing a value of this type.
         public static final int vishalika=0x7f020026;
     }
     public static final class id {
-        public static final int action_contact=0x7f09000a;
-        public static final int button=0x7f090009;
+        public static final int action_contact=0x7f090009;
+        public static final int button=0x7f090008;
         public static final int center=0x7f090006;
         public static final int colors=0x7f090004;
         public static final int dishName=0x7f090001;
         public static final int image=0x7f090005;
         public static final int pager=0x7f090003;
         public static final int picNname=0x7f090000;
-        public static final int scrollView1=0x7f090007;
-        public static final int shekhar=0x7f090008;
+        public static final int shekhar=0x7f090007;
         public static final int tabs=0x7f090002;
     }
     public static final class layout {
--- sample/src/com/astuetz/viewpager/extensions/sample/indexMain.java
+++ sample/src/com/astuetz/viewpager/extensions/sample/indexMain.java
@@ -6,17 +6,20 @@ import android.app.Activity;
 import android.content.Intent;
 import android.view.View;
 import android.view.View.OnClickListener;
-public class indexMain extends Activity {
+
+public class IndexMain extends Activity  implements OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.index);
         Button nextButton = (Button) findViewById(R.id.button);
-        nextButton.setOnClickListener((OnClickListener) this);
+        nextButton.setOnClickListener(this);
     }
+    
+    @Override
     public void onClick(View view) {
-        Intent intent=new Intent(indexMain.this,MainActivity.class);
+        Intent intent=new Intent(IndexMain.this,MainActivity.class);
         startActivity(intent);
     }
 }
